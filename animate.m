2e5e33b2367f6bbbb06e270cbf99fd1683c9213f
JackLinehan <35932421+JackLinehan@users.noreply.github.com>
2018-12-20 14:10:25 -0600

    Update animate.m

--- animate.m
+++ animate.m
@@ -30,8 +30,11 @@ end
 clear vars my_area my_intensity my_centroids my_perimeter my_outline k my_data
 %% Animate the Cells 
 f = figure(1); 
-axis([0 500 0 475]); 
-set(gca,'color','k'); 
+axis([0 1.2 -1.5699 -1.563 0 250000]);
+view([107 52]);  
+
+set(gca,'color',[.5 .5 .5]); 
+grid on
 N = abs(length(area_all{1,1})); 
 x = zeros(n,N); 
 y = zeros(n,N); 
@@ -42,32 +45,56 @@ for j = 1:N-1
     y(:,j) = centroids(:,1); 
     
     outlines = cellfun(@(x) x{:,j},outline_all,'UniformOutput',false); 
-    
+
     for m = 1:n
         
         hold on 
         
         x_1 = outlines{m,1}(:,2); 
         y_1 = outlines{m,1}(:,1);
+        z = sqrt(1-(x_1.^2 + y_1.^2)); 
+        z = z.^2; 
         
-        plot(y_1,x_1,'g'); 
+        [az,elev,r] = cart2sph(x_1,y_1,z); 
+        plot3(az, elev,r,'g'); 
         ax = f.Children; 
         b = ax.Children; 
         hold off 
+
     end 
-        
-    
     hold on 
-    plot(x(:,1:j),y(:,1:j),'o');
-    hold off; 
+    y_2 = x(:,1:j); % 1:j to include previous points
+    x_2 = y(:,1:j); 
     
+    z_2 = sqrt(1-(x_2.^2 + y_2.^2)); 
+    z_2 = (z_2).^2; 
+    [az_1, elev_1, r_1] = cart2sph(x_2,y_2,z_2);  
+    
+    plot3(az_1,elev_1,r_1,'.');
+    hold off; 
+    drawnow 
     disp(j); 
-    pause(0.13); 
+    pause(1); 
+    
+    
+    frame = getframe(f); % save the image in the figure 
+    im = frame2im(frame); % convert that frame into a true image
+    [imind, cm] = rgb2ind(im,256); % add some color to it 
+    
+    % Put the color image we just generated in the frame into a gif 
+    if (j == 1)
+        imwrite(imind, cm,'my_gif.gif','Loopcount',inf); 
+    else 
+        imwrite(imind, cm, 'my_gif.gif','WriteMode','append'); 
+    end 
     
     if (j < N-1)
     delete(b); 
     end 
 end 
+
+
+
 
 %% Let's look at some measures, change in area n'position 
  
@@ -122,8 +149,6 @@ for j = 1:abs(length(area_all))
 
 
 end 
-
-
 
 
 
